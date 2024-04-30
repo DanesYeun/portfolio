610b6ea5c71b326fc2ad66fdf2175dc57760385d
Danes Yaun <dadadanesss@gmail.com>
2024-04-30 10:16:29 +0800

    mainpage(): minor adjustments

--- src/pages/Mainpage.jsx
+++ src/pages/Mainpage.jsx
@@ -20,7 +20,7 @@ const Mainpage = () => {
 
     return(
         <div className="w-screen h-screen">       
-            <div className="mx-auto md:max-w-2xl h-16">
+            <div className="mx-auto md:max-w-2xl h-16 p-2">
                <Navbar activeTab={activeTab} setActiveTab={setActiveTab}/>
             </div>
             <div className="border rounded-xl bg-blue-400 mx-auto md:max-w-2xl md:h-4/5 mt-2">
